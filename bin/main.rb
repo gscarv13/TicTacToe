c983f75578fd31f83625ba303b99f486f9f82eb1
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-01-12 19:01:09 -0300

    Revert "Move classes into game_logic file"
    This reverts commit dcea19404759bb9e9533b102fd9be184a4639255.

--- bin/main.rb
+++ bin/main.rb
@@ -1,6 +1,24 @@
 #!/usr/bin/env ruby
 
-require_relative '../lib/game_logic'
+# Classes
+
+class Board
+  def print_board
+    puts "[ #{Square.new('1')} ] [ #{Square.new('2')} ] [ #{Square.new('3')} ]"
+    puts "[ #{Square.new('4')} ] [ #{Square.new('5')} ] [ #{Square.new('6')} ]"
+    puts "[ #{Square.new('7')} ] [ #{Square.new('8')} ] [ #{Square.new('9')} ]"
+  end
+end
+
+class Square
+  def initialize(char = nil)
+    @char = char
+  end
+
+  def to_s
+    @char
+  end
+end
 
 # Printing
 

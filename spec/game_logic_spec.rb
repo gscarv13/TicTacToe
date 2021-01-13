2ef2d4fd6084cae176e53955ce9544a8304e7b12
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-01-13 17:00:13 -0300

    Add more rspec examples

--- spec/game_logic_spec.rb
+++ spec/game_logic_spec.rb
@@ -16,14 +16,20 @@ draw_board.change_box('8', player2)
 
 describe Board do
   describe '#check_input' do
+    it 'checks valid cases' do
+      (1..9).map { |number| expect(Board.new.check_input(number.to_s)).to eql(true) }
+      (1..9).map { |number| expect(Board.new.check_input("    #{number}")).to eql(true) }
+      (1..9).map { |number| expect(Board.new.check_input("#{number}      ")).to eql(true) }
+    end
     it 'checks for invalid input' do
       expect(Board.new.check_input('0')).to eql(false)
       expect(Board.new.check_input('10')).to eql(false)
       expect(Board.new.check_input('a')).to eql(false)
       expect(Board.new.check_input(' ')).to eql(false)
+      expect(Board.new.check_input('a 1')).to eql(false)
+      expect(Board.new.check_input('1 2')).to eql(false)
     end
     it 'checks for overwritting attempts' do
-      (1..9).map { |number| expect(Board.new.check_input(number.to_s)).to eql(true) }
       (1..9).map { |number| expect(draw_board.check_input(number.to_s)).to eql(false) }
     end
   end

d13c11dc40f70c1a7617b3db34e42df14f45e153
pipe2442 <felipeangulo2442@gmail.com>
2021-01-15 16:14:13 -0500

    Created let(:board) var for testing

--- spec/game_logic_spec.rb
+++ spec/game_logic_spec.rb
@@ -31,13 +31,12 @@ draw.change_box('9', player1)
 
 describe Board do
   describe '#check_input' do
+    let(:board) { Board.new }
     it 'returns false if input == 0' do
-      board = Board.new
       expect(board.check_input(0)).to be false
     end
 
     it 'returns false unless input.positive? && input < 10' do
-      board = Board.new
       expect(board.check_input(-2)).to be false
       expect(board.check_input(12)).to be false
       expect(board.check_input(5)).to be true
